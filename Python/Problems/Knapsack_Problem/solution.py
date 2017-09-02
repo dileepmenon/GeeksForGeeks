843718a7dc22758615286d60bec3015f80f2c60a
Dileep Menon <dileepmenon92@yahoo.com>
2017-09-02 12:20:34 +0530

    Add Correction

--- Python/Problems/Knapsack_Problem/solution.py
+++ Python/Problems/Knapsack_Problem/solution.py
@@ -1,6 +1,8 @@
 def KP(item_val, item_wt, w_left):
     if w_left <= 0 or not item_val:
         return 0
+    elif w_left-item_wt[0] <= 0:
+        return KP(item_val[1:], item_wt[1:], w_left)
     else:
         return (max(item_val[0]
                     + KP(item_val[1:], item_wt[1:], w_left-item_wt[0]),
