839b8b13d5aee465f3e97b74ab8d2e851efab640
Dileep Menon <dileepmenon92@yahoo.com>
2017-09-10 00:34:21 +0530

    Recursive Solution with Memoization

--- Python/Problems/Stickler_Thief/solution.py
+++ Python/Problems/Stickler_Thief/solution.py
@@ -1,16 +1,22 @@
 #!bin/python3
+
+
 def MS(arr_h):
     if len(arr_h) == 1:
         return arr_h[0]
-    if not arr_h[2:]:
-        return arr_h[0]
-    else:
-        return max(arr_ h[0] + MS(arr_h[2:]),
-                   MS(arr_h[1:]))
+    if not arr_h:
+        return 0
+    if len(arr_h) <= len(MS_Array):
+        return MS_Array[len(arr_h) - 1]
+    t = max(arr_h[0] + MS(arr_h[2:]),
+            MS(arr_h[1:]))
+    MS_Array.append(t)
+    return t
 
 
 num_t = int(input())
 for i in range(num_t):
-    num_h = int(input().strip())
-    arr_h = list(map(int, input().strip().split()))
+    MS_Array = [0]
+    num_h = int(input())
+    arr_h = list(map(int, input().split()))
     print(MS(arr_h))
