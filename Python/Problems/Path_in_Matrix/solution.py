f1630d049dfe30c6af352dfe14a5e2dbe34374cd
Dileep Menon <dileepmenon92@yahoo.com>
2017-11-14 14:15:01 +0530

    Refactoring

--- Python/Problems/Path_in_Matrix/solution.py
+++ Python/Problems/Path_in_Matrix/solution.py
@@ -1,38 +1,29 @@
 #!bin/python3
 
 
-def getmaxpath(mat, r, c, k, tab, n):
-    if k < 0:
+def get_sum(arr, n, i, j):
+    if i == n-1:
         return 0
-    if tab[r][c] > 0:
-        return tab[r][c]
-    else:
-        if r+1 >= n:
-            a1, a2, a3 = -1, -1, -1
-        else:
-            if c-1 < 0:
-                a1, a2, a3 = -1, mat[r+1][c], mat[r+1][c+1]
-            elif c+1 >= n:
-                a1, a2, a3 = mat[r+1][c-1], mat[r+1][c], -1
-            else:
-                a1, a2, a3  = mat[r+1][c-1], mat[r+1][c], mat[r+1][c+1]
-
-        s = max(k + getmaxpath(mat, r+1, c-1, a1, tab, n),
-                k + getmaxpath(mat, r+1, c, a2, tab, n),
-                k + getmaxpath(mat, r+1, c+1, a3, tab, n))
-        tab[r][c] = s
+    try:
+        return d[(i, j)]
+    except:
+        s = max(arr[i+1][j-1] + get_sum(arr, n, i+1, j-1) if j-1 >= 0 else 0,
+                arr[i+1][j] + get_sum(arr, n, i+1, j),
+                arr[i+1][j+1] + get_sum(arr, n, i+1, j+1) if j+1 < n else 0
+                )
+        d[(i, j)] = s
         return s
 
 
 num_t = int(input())
 for i in range(num_t):
     n = int(input())
-    l = list(map(int, input().strip().split(' ')))
-    mat = [l[i:i+n] for i in range(0, len(l)-n+1, n)]
-    path_sum = []
-    tab = []
+    m = list(map(int, input().strip().split()))
+    arr = []
+    for i in range(0, len(m), n):
+        arr.append(m[i:i+n])
+    max_l_path = []
+    d = {}
     for i in range(n):
-        tab.append([0]*n)
-    for i in range(len(mat[0])):
-        path_sum.append(getmaxpath(mat, 0, i, mat[0][i], tab, n))
-    print(max(path_sum))
+        max_l_path.append(arr[0][i] + get_sum(arr, n, 0, i))
+    print(max(max_l_path))
